2e102141a68e59a73858d62ef83ff8159368ae2d
Sheralyn Files <gired0329@yahoo.com>
2021-11-29 09:34:27 -0500

    Updates

---
+++ precache-manifest.a9b88c0d353648df8971a5f115cef653.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1247cb91a282a76ff854583b63c76eea",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "5bc0cfbcdb1e06fb5a3f",
+    "url": "/React-Portfolio/static/css/main.ccd255e5.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/React-Portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bc0cfbcdb1e06fb5a3f",
+    "url": "/React-Portfolio/static/js/main.b5b58952.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  },
+  {
+    "revision": "90948b6e570f4622bc8124328b2ddf54",
+    "url": "/React-Portfolio/static/media/Budget-Tracker.90948b6e.jpg"
+  },
+  {
+    "revision": "25029d493f41060ff5b34bdcd46d1321",
+    "url": "/React-Portfolio/static/media/Code Quiz.25029d49.jpg"
+  },
+  {
+    "revision": "4bc88694ff7ac275e073d42954a8d94c",
+    "url": "/React-Portfolio/static/media/Easy_Stack.4bc88694.jpg"
+  },
+  {
+    "revision": "3f95334c23b1b6da46574d5e8347e731",
+    "url": "/React-Portfolio/static/media/Note-Taker.3f95334c.jpg"
+  },
+  {
+    "revision": "4ccb76e515042cfc5f330c9924a1b73f",
+    "url": "/React-Portfolio/static/media/Unearthly Venues.4ccb76e5.jpg"
+  },
+  {
+    "revision": "c0f157ab3de22874de48ccc165d0667b",
+    "url": "/React-Portfolio/static/media/cover-image.c0f157ab.jpg"
+  },
+  {
+    "revision": "6e87436e2764bc446a4f76eec2d7e49c",
+    "url": "/React-Portfolio/static/media/work_day_scheduler.6e87436e.jpg"
+  }
+]);
